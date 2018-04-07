393fc7ffb281f68a289f1b13e65a716273b6c75e
Josselin Giet <giet@clipper.ens.fr>
2018-04-07 20:31:52 +0200

    Removing z3 oracles from travis tests

--- test.sh
+++ test.sh
@@ -9,12 +9,12 @@ for filename in ./data/InToHyLo/test*; do
 	if [ $? != 0 ]
 		then exit 1
 	fi
-	./moloss.native "$filename" --time --z3
-	if [ $? != 0 ]
-		then exit 1
-	fi
-	./moloss.native "$filename" --time --direct
-	if [ $? != 0 ]
-		then exit 1
-	fi
+	# ./moloss.native "$filename" --time --z3
+	# if [ $? != 0 ]
+	# 	then exit 1
+	# fi
+	# ./moloss.native "$filename" --time --direct
+	# if [ $? != 0 ]
+	# 	then exit 1
+	# fi
 done
