abcae35047bdbd1e8b200242f6aa2b5acb958f75
Josselin Giet <giet@clipper.ens.fr>
2018-05-22 21:20:03 +0200

    upadting doc

--- myocamlbuild.ml
+++
@@ -1,9 +0,0 @@
-open Ocamlbuild_plugin
-
-let () =
-  dispatch begin function
-    | After_hygiene ->
-      if getenv "STATIC" ~default:"false" = "true" then
-        tag_any ["ccopt(-static)"]
-    | _ -> ()
-  end
